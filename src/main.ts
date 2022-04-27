634f016f7a7bc14ee1d23f230dd4f07ba8717d58
luizaoformigapqd <luizlima@garantti.com.br>
2022-04-27 20:19:01 -0300

    add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions } from '@nestjs/microservices';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { grpcClientOptions } from 'grpc-client.options';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+
+  const config = new DocumentBuilder()
+    .setTitle('HEROES API')
+    .setDescription('The heroes API description')
+    .setVersion('1.0')
+    .addTag('heroes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.startAllMicroservices();
   await app.listen(3001);
